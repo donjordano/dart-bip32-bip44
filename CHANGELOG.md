a8700e2ff3aab839a8ca19ef9a893bfe7dddd25d
donjordano <jordanov.ivan@gmail.com>
2023-01-03 14:49:03 +0200

    Update SDK and remove Flutter as dependecie

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+v.0.2.1
+*Froked. Removed Flutter as dependecie and the SDK
+
 v0.2.0
 * Forked from abandoned repo and updated to work with latest dependencies and null-safety because I need this library for a project
 
--- example/main.dart
+++ example/main.dart
@@ -3,7 +3,11 @@ import 'package:convert/convert.dart';
 import 'package:dart_bip32_bip44/dart_bip32_bip44.dart';
 
 void main() {
-  var chain = Chain.seed(hex.encode(utf8.encode('some seed')));
-  var key = chain.forPath('m/0/100') as ExtendedPrivateKey;
-  print(key);
+  var chain = Chain.seed(hex.encode(utf8.encode(
+      'tower slice pond crunch tower viable enroll witness enough number noble exchange')));
+  ExtendedPrivateKey privateKey =
+      chain.forPath("m/44'/60'/0'/0") as ExtendedPrivateKey;
+  print(privateKey.toString());
+  print(privateKey.key);
+  print(privateKey.publicKey());
 }
